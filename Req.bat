a97dc87f28c2c5a4442a542ee3166b1d0131bc9a
Remote Tools <102330733+redriver123@users.noreply.github.com>
2024-09-28 17:57:32 -0400

    Req.bat

---
+++ Req.bat
@@ -0,0 +1,34 @@
+@echo off
+title Requirements Installer
+color 0a
+echo This tool installs all the requirements needed for the BossRatKit!       
+
+pause
+
+pip install discord.py==2.1.0
+pip install pynput==1.7.6
+pip install pillow==10.0.1
+pip install filesplit==4.0.1
+pip install pyaudio==0.2.12
+pip install sounddevice==0.4.5
+pip install scipy==1.9.3
+pip install pyautogui==0.9.53
+pip install pynacl==1.5.0
+pip install psutil==5.9.4
+pip install pypiwin32==223
+pip install pycryptodome==3.15.0
+pip install requests==2.28.1
+pip install browser_history==0.3.2
+pip install pygame
+pip install imageio
+pip install imageio[ffmpeg]
+pip install numpy
+pip install pynput
+pip install pyperclip
+pip install html2image
+pip install pyttsx3
+pip install pycaw
+pip install comtypes
+pip install monitorcontrol
+pip install cryptography
+pip installpypiwin32
